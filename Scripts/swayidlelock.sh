b5a6738ee0dae5d4fd26bab285c3475a9d5dd25e
sauravdhakal12 <sauravdhakal121@gmail.com>
2024-06-30 09:07:12 +0545

    Swayidle hibernate to suspend

--- Scripts/swayidlelock.sh
+++ Scripts/swayidlelock.sh
@@ -1,4 +1,5 @@
 swayidle -w \
   timeout 180 'swaylock -f' \
   timeout 240 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' \
+  timeout 300 'systemctl suspend' \
   before-sleep 'swaylock -f'
